803f354b5df41a7ee36e63160b7a4a6f64506720
revin99 <revingohil@gmail.com>
2025-01-05 12:19:22 +0530

    Configured postgres

--- expense_manager/expense_manager/settings.py
+++ expense_manager/expense_manager/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -72,11 +73,14 @@ WSGI_APPLICATION = 'expense_manager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+load_dotenv()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD':os.getenv('DB_USER_PASSWORD'),
+        'HOST':os.getenv('DB_HOST_PASSWORD'),
     }
 }
 
